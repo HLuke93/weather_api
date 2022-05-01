f9fc10af8b62a89927c4e94edd0b49d12c029bd9
HLuke93 <lukehickson2020@gmail.com>
2022-05-01 13:56:11 +0000

    add api key and user input ,  added api link

--- run.py
+++ run.py
@@ -1,2 +1,12 @@
+import requests
+from datetime import datetime
 
+api_key = "0a83a5d02d85430986aa98fc001d9504"
+location = input("Please enter the city name: ")
 
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+print(api_data)
